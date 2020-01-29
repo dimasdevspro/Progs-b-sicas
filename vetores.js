b59eaf0f771ec00b46f015dd3e861eaa91d9b494
Dimas Alves Pereira <dimasdevspro@gmail.com>
2020-01-29 10:14:53 -0300

    added vetores.js

---
+++ vetores.js
@@ -0,0 +1,10 @@
+const programador = {
+    nome: 'Dimas',
+    idade: 39,
+    tecnologias: [
+        {nome: 'JavaScript', especialidade: 'Web/Mobile'},
+        {nome: 'HTML5', especialidade: 'Web/Mobile'},
+        {nome: 'CSS', especialidade: 'Web/Mobile'},
+    ]
+    }
+console.log (`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[0].nome} com especialidade em ${programador.tecnologias[0].especialidade} `)
